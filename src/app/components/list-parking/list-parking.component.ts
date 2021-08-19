fad2450be543861dce5e87e6d8d334a7bb42cfbf
David Montoya Ramírez <deividelpaisa@hotmail.com>
2021-08-19 17:16:26 -0500

    tabla hecha

--- src/app/components/list-parking/list-parking.component.ts
+++ src/app/components/list-parking/list-parking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICars } from 'src/app/interfaces/Cars';
 
 @Component({
   selector: 'app-list-parking',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-parking.component.css']
 })
 export class ListParkingComponent implements OnInit {
+
+  listCars: ICars[] = [
+    {
+    id: 123,
+    placa: "AEF-345",
+    hora_entrada: new Date("Fri Dec 08 2019 07:44:57"),
+    hora_salida: new Date("Fri Dec 08 2019 07:44:57"),
+    estado: false,
+    total: 24000
+  },
+  {
+    id: 321,
+    placa: "KHY-645",
+    hora_entrada: new Date("Fri Dec 08 2019 09:44:45"),
+    hora_salida: new Date("Fri Dec 08 2021 09:35:57"),
+    estado: false,
+    total: 24000
+  }
+]
 
   constructor() { }
 
