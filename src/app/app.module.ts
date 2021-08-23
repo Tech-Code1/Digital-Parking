33cedc09617e0dd5db20ee790f5deed3c0e96262
David Montoya Ramírez <deividelpaisa@hotmail.com>
2021-08-23 14:53:10 -0500

    Formulario creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ListParkingComponent } from './components/list-parking/list-parking.com
 import { ViewCarComponent } from './components/view-car/view-car.component';
 import { AddEditParkingComponent } from './components/add-edit-parking/add-edit-parking.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-edit-parking/add-edit-parking.component.ts
+++ src/app/components/add-edit-parking/add-edit-parking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ICars } from 'src/app/interfaces/Cars';
 
 @Component({
   selector: 'app-add-edit-parking',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditParkingComponent implements OnInit {
 
-  constructor() { }
+  addCar: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.addCar = this.fb.group({
+      placa: ["", Validators.required]
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  add() {
+    console.log(this.addCar);
+    const car: ICars = {
+      id: 0,
+      placa: this.addCar.get("placa")?.value,
+      estado: true,
+      hora_entrada: new Date,
+      hora_salida: new Date,
+      total: 0
+    };
+
+    console.log(car);
+
   }
 
 }
